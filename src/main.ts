b8b74db523d82931e6067e7dacddb6d4e32bea39
Indonesia Scripts <tugas137@gmail.com>
2026-06-21 11:55:14 +0700

    Update main.ts
    menambahkan akses cors ke url railway dari vercel production

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: 'http://localhost:3001',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true,
-  });
+  // Sesudah (support both production & local)
+app.enableCors({
+  origin: [
+    'http://localhost:3001',
+    'https://solusiklik-crm.vercel.app',
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
+});
 
   await app.listen(3000);
 }
